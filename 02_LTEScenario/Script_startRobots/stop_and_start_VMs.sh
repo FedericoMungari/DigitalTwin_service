b92c2be4fb3b47c814cccd3520d1691a0c32ed40
Federico Mungari <federico.mungari@polito.it>
2022-01-18 17:21:13 +0100

    settings

--- 02_LTEScenario/Script_startRobots/stop_and_start_VMs.sh
+++ 02_LTEScenario/Script_startRobots/stop_and_start_VMs.sh
@@ -2,7 +2,7 @@
 
 # VMs STOPPING
 echo -e "\n..Stopping the robot driver"
-sshpass -p${ROBOT_HOST_PASS} ssh $ROBOT_HOST_USER@$ROBOT_HOST_IP "VBoxManage controlvm ROS_VNF_driver poweroff" &>/dev/null
+sshpass -p${ROBOT_HOST_PASS} ssh $ROBOT_HOST_USER@$ROBOT_HOST_IP "VBoxManage controlvm ROS_VNF_driver poweroff" # &>/dev/null
 sleep  2
 echo -e "\n..Stopping the robot control"
 VBoxManage controlvm ROS_VNF_control poweroff &>/dev/null
@@ -23,7 +23,7 @@ sleep 3
 
 # VMs STARTING
 echo -e "\n..Starting the robot driver"
-sshpass -p${REMOTE_HOST_PASS} ssh $REMOTE_HOST_USER@$REMOTE_HOST_IP "VBoxManage startvm ROS_VNF_driver --type headless" &>/dev/null
+sshpass -p${REMOTE_HOST_PASS} ssh $REMOTE_HOST_USER@$REMOTE_HOST_IP "VBoxManage startvm ROS_VNF_driver --type headless" # &>/dev/null
 sleep  1
 echo -e "\n..Starting the robot control"
 VBoxManage startvm ROS_VNF_control --type headless &>/dev/null
